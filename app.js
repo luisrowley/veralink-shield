84464b50bbc2ed7160c4f044df12cd93e5d8f1c2
luisrowley <louisdebenito@gmail.com>
2023-04-28 19:03:14 +0200

    created key-generator dedicated route.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const verifyRouter = require('./src/verifier/verify');
+const keyGenRouter = require('./src/key-generator/key-generator');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/verify', verifyRouter);
+app.use('/key-generator', keyGenRouter);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
