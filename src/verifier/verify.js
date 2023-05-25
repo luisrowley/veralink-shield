62d7764e10145725240083312b60b61d35b51a8f
luisrowley <louisdebenito@gmail.com>
2023-05-25 21:21:47 +0200

    included use case for direct mobile request.

--- src/verifier/verify.js
+++ src/verifier/verify.js
@@ -1,18 +1,29 @@
 const express = require('express');
-const router = express.Router();
 const crypto = require('crypto');
 const request = require('request');
 const fs = require('fs');
+const isValidHttpUrl = require('../utils/data-validators');
+
+const router = express.Router();
 
 const VERALINK_PRIVATE_KEY = fs.readFileSync('private.key');
 const REMOTE_ENDPOINT_URI = '/veralink';
 
 router.get('/', (req, res) => {
   const urlToVerify = req.query.url;
-  const REMOTE_SDK_URL = urlToVerify + REMOTE_ENDPOINT_URI;
+  const mobileReq = req.query.mobile;
+  const urlParams = new URL(urlToVerify);
+  const host = `${urlParams.protocol}//${urlParams.host}`;
+  const REMOTE_SDK_URL = host + REMOTE_ENDPOINT_URI;
+  console.log(`host: ${host}`, `mobile: ${mobileReq}`);
 
   if (!urlToVerify) {
-    res.status(400).send('Bad Request: missing url parameter.');
+    res.status(400).send('(404) Bad Request: missing url parameter.');
+    return;
+  }
+  // check remote host url
+  if (!isValidHttpUrl(host)) {
+    res.status(400).send('(404) Bad Request: unable to resolve remote host.');
     return;
   }
 
@@ -20,31 +31,36 @@ router.get('/', (req, res) => {
   signature.write(urlToVerify);
   signature.end();
 
-  console.log(urlToVerify, REMOTE_SDK_URL);
+  // console.log(urlToVerify, REMOTE_SDK_URL);
 
   const signatureBase64 = signature.sign(VERALINK_PRIVATE_KEY, 'base64');
 
-  request.post(REMOTE_SDK_URL, {
-    form: {
-      url: urlToVerify,
-      signature: signatureBase64
-    }
-  }, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.log('Failed to verify URL signature' + error, signature);
-      res.status(500).send('Failed to verify URL signature');
-      return;
-    }
+  // send verification to remote SDK
+  if (mobileReq) {
+    request.post(REMOTE_SDK_URL, {
+      form: {
+        url: urlToVerify,
+        signature: signatureBase64,
+      },
+    }, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.log(`Failed to verify URL signature${error}`, signature);
+        res.status(500).send('Failed to verify URL signature');
+        return;
+      }
 
-    if (body !== 'OK') {
-      console.log('Invalid signature');
-      res.status(500).send('Invalid signature');
-      return;
-    }
+      if (body !== 'OK') {
+        console.log('Invalid signature');
+        res.status(500).send('Invalid signature');
+        return;
+      }
 
-    console.log(`URL redirect: ${urlToVerify}`);
-    res.redirect(urlToVerify);
-  });
+      console.log(`URL redirect: ${urlToVerify}`);
+      res.redirect(urlToVerify);
+    });
+  } else {
+    // send user to QR code page
+  }
 });
 
 module.exports = router;
