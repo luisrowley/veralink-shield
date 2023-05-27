b05bbc7d31d60ffa53c5f925d5980c0225242fb0
luisrowley <louisdebenito@gmail.com>
2023-05-27 14:23:46 +0200

    removed debugging messages.

--- src/verifier/verify.js
+++ src/verifier/verify.js
@@ -19,7 +19,6 @@ router.get('/', (req, res) => {
   const urlParams = new URL(urlToVerify);
   const host = `${urlParams.protocol}//${urlParams.host}`;
   const REMOTE_SDK_URL = host + REMOTE_ENDPOINT_URI;
-  console.log(`host: ${host}`);
 
   if (!urlToVerify) {
     res.status(400).send('(404) Bad Request: missing url parameter.');
@@ -35,13 +34,9 @@ router.get('/', (req, res) => {
   signature.write(urlToVerify);
   signature.end();
 
-  // console.log(urlToVerify, REMOTE_SDK_URL);
-
   const signatureBase64 = signature.sign(VERALINK_PRIVATE_KEY, 'base64');
 
   // send verification to remote SDK
-  // if request contains channelID, send validate req to SDK
-  // validate OK? -> resolve matching channelID and redirect
 
   request.post(REMOTE_SDK_URL, {
     form: {
@@ -50,18 +45,15 @@ router.get('/', (req, res) => {
     },
   }, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      console.log(`Failed to verify URL signature${error}`, signature);
       res.status(500).send('Failed to verify URL signature');
       return;
     }
 
     if (body !== 'OK') {
-      console.log('Invalid signature');
       res.status(500).send('Invalid signature');
       return;
     }
 
-    console.log(`URL redirect: ${urlToVerify}`);
     res.redirect(urlToVerify);
   });
 });
